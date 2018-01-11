fbea5517461504842dffb445400d6210b57b8052
Ankits29 <ankits2912@gmail.com>
2018-01-11 17:10:01 +0530

    added routes for add student and find student by id

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ app.get('/api/users/all', function(req, res){
 	})
 });
 
+app.post('/api/user/add', function(req, res){
+	var student = models.student.build({
+		userID : req.body.userID,
+		name : req.body.name,
+		email : req.body.email,
+		phoneNumber : req.body.phoneNumber,
+		address : req.body.address
+	});
+	student.save().then(function(newStudent){
+		res.json(newStudent)	
+	}) 
+})
+
+app.post('/api/user/findone', function(req, res){
+	console.log('@@@@', req.body.id)
+	models.student.findById(req.body.id).then(function(student){
+		res.json(student)
+	})
+});
+
 app.listen(3000, function(){
 	console.log('server listining to port 3000...')
 })
